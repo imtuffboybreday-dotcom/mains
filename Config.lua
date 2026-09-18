a8995cdcc0f2b42b9cd180ab20388122f5c3f190
imtuffboybreday-dotcom <imtuffboybreday@gmail.com>
2026-09-18 22:15:59 +0700

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,12 @@
+return {
+    Version = "3.0.0",
+    OwnerUserId = 0,
+    CheckInterval = 2.5,
+    Integrity = {
+        StageOne = "A7F29C",
+        StageTwo = "D41B8E",
+        StageThree = "93C6FA",
+        RuntimeId = "5E17D2"
+    },
+    Enabled = true
+}
